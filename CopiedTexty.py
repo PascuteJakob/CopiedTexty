3908e9c2c872ef9efb3c317980272128d2fd5f42
PascuteJakob <115586882+PascuteJakob@users.noreply.github.com>
2023-02-07 13:30:47 -0500

    read data file at launch
    read data file at launch

--- CopiedTexty.py
+++ CopiedTexty.py
@@ -3,8 +3,12 @@ sg.theme('DarkAmber')
 import keyboard
 import mouse
 from threading import Thread
-  
+
 savedTexts = []
+f = open('CopiedTextyData.csv', 'r')
+for lines in f:
+	savedTexts.append(lines)
+print(savedTexts)
 modifiers = ['ctrl', 'shift', 'alt']
 keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
