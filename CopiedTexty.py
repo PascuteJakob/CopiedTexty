880c369d22238c1fc709ad806769b583ec73454f
PascuteJakob <115586882+PascuteJakob@users.noreply.github.com>
2023-02-07 11:22:22 -0500

    More less-dogshit-ifying
    More less-dogshit-ifying

--- CopiedTexty.py
+++ CopiedTexty.py
@@ -5,18 +5,24 @@ from threading import Thread
     
 def GuiFunc():
 	sg.theme('DarkAmber')
-	tab1_layout = [[sg.Text('My first tab.'), sg.Text('poop')]]    
 
-	tab2_layout = [[sg.Text('My Second tab.'), sg.Text('poop')]]
+	tab1_layout = [[
+		sg.Text('My first tab.'),
+		sg.Text('poop'),
+	]]    
+	tab2_layout = [[
+		sg.Text('My Second tab.'),
+		sg.Text('poop'),
+	]]
 
 	layout_tabgroup = [[
 		sg.Tab('CopiedTexty', tab1_layout, tooltip='tip'),
 		sg.Tab('Hotkeys', tab2_layout, tooltip='tip2'),
 	]]
+
 	layout = [[
 		sg.TabGroup(
 			layout_tabgroup,
-
 			)
 	]]
 	window = sg.Window('Window Title', layout)    
