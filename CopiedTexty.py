df47a5254fcec4ac053f53a4e05c12e69a07ce5e
PascuteJakob <115586882+PascuteJakob@users.noreply.github.com>
2023-02-07 12:47:10 -0500

    Second Window added
    Second Window added

--- CopiedTexty.py
+++ CopiedTexty.py
@@ -1,29 +1,36 @@
 import PySimpleGUI as sg
+sg.theme('DarkAmber')
 import keyboard
 import mouse
 from threading import Thread
   
-savedTexts = ["test", "test2", "test3"]
+savedTexts = []
 
 class Gui:
 	def __init__(self):
 		self.mainWin()
-		self.window = sg.Window('Copied Texty', self.layout) 
+		self.newWin()
+		self.mainWin = sg.Window('Copied Texty', self.mainWin_layout,
+ 			finalize = True) 
+		self.mainWinLocation = self.mainWin.CurrentLocation()
+		print(self.mainWinLocation)
+		self.newWin = sg.Window('Add Entry', self.newWin_layout,
+			location=(self.mainWinLocation[0]+228,self.mainWinLocation[1]), finalize=True,)
+		self.newWin.Hide()
 	def mainWin(self):
-		sg.theme('DarkAmber')
 		tab1_layout = [[
 			sg.Text('Texty'),
 			sg.Text('Hotkey', p=((80,0),(0,0))),
 		],[
 			sg.Listbox(savedTexts,
-			default_values = "No saved texts",
+			default_values = None,
 			size=(24,10),
 			key='__textEntry__'),
 			
 		],[
-			sg.Button('New', s=(6,1),p=((5,4),(0,0))),
-			sg.Button('Edit', s=(6,1)),
-			sg.Button('Delete', s=(6,1))
+			sg.Button('New', s=(6,1),p=((5,4),(0,0)), key='__New__'),
+			sg.Button('Edit', s=(6,1), key='__Edit__'),
+			sg.Button('Delete', s=(6,1), key='__Delete__')
 		]]
 		tab2_layout = [[
 			sg.Text('My Second tab.'),
@@ -40,17 +47,25 @@ class Gui:
 			sg.Tab('About', tab3_layout)
 		]]
 
-		self.layout = [[
+		self.mainWin_layout = [[
 			sg.TabGroup(
 				layout_tabgroup,
 				)
 		]]
 
+	def newWin(self):
+		self.newWin_layout = [[
+			sg.Text('Input your new Texty here'),
+		]]
+
 	def mainLoop(self):
 		while True:
-			event, values = self.window.read() 
+			event, values = self.mainWin.read() 
 			if event == sg.WIN_CLOSED:
 				break
+			if event == '__New__':
+				self.newWin.UnHide()
+
 
 def main():
 	myWin = Gui()
