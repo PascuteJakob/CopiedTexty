f0b139600fe2bb523c2a3a4d93eaf0beb2ccfbcb
PascuteJakob <115586882+PascuteJakob@users.noreply.github.com>
2023-02-07 12:20:24 -0500

    Added Gui class and cleaned up code
    Added Gui class and cleaned up code

--- CopiedTexty.py
+++ CopiedTexty.py
@@ -2,38 +2,60 @@ import PySimpleGUI as sg
 import keyboard
 import mouse
 from threading import Thread
-    
-def GuiFunc():
-	sg.theme('DarkAmber')
+  
+savedTexts = ["test", "test2", "test3"]
 
-	tab1_layout = [[
-		sg.Text('My first tab.'),
-		sg.Text('poop'),
-	]]    
-	tab2_layout = [[
-		sg.Text('My Second tab.'),
-		sg.Text('poop'),
-	]]
+class Gui:
+	def __init__(self):
+		self.mainWin()
+		self.window = sg.Window('Copied Texty', self.layout) 
+	def mainWin(self):
+		sg.theme('DarkAmber')
+		tab1_layout = [[
+			sg.Text('Texty'),
+			sg.Text('Hotkey', p=((80,0),(0,0))),
+		],[
+			sg.Listbox(savedTexts,
+			default_values = "No saved texts",
+			size=(24,10),
+			key='__textEntry__'),
+			
+		],[
+			sg.Button('New', s=(6,1),p=((5,4),(0,0))),
+			sg.Button('Edit', s=(6,1)),
+			sg.Button('Delete', s=(6,1))
+		]]
+		tab2_layout = [[
+			sg.Text('My Second tab.'),
+			sg.Text('poop'),
+		]]
+		tab3_layout = [[
+			sg.Text('My Second tab.'),
+			sg.Text('poop'),
+		]]
 
-	layout_tabgroup = [[
-		sg.Tab('CopiedTexty', tab1_layout, tooltip='tip'),
-		sg.Tab('Hotkeys', tab2_layout, tooltip='tip2'),
-	]]
+		layout_tabgroup = [[
+			sg.Tab('CopiedTexty', tab1_layout),
+			sg.Tab('Settings', tab2_layout),
+			sg.Tab('About', tab3_layout)
+		]]
 
-	layout = [[
-		sg.TabGroup(
-			layout_tabgroup,
-			)
-	]]
-	window = sg.Window('Window Title', layout)    
+		self.layout = [[
+			sg.TabGroup(
+				layout_tabgroup,
+				)
+		]]
 
-	while True:
-		event, values = window.read() 
-		if event == sg.WIN_CLOSED:
-			break
+	def mainLoop(self):
+		while True:
+			event, values = self.window.read() 
+			if event == sg.WIN_CLOSED:
+				break
 
-
+def main():
+	myWin = Gui()
+	myWin.mainLoop()
 
 if __name__ == "__main__":
-	GuiFunc()
+	main()
 
